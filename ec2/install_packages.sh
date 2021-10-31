a7e8e861c12d991cfedcb189fb9309f25c22aa10
TaiseiIto <taisei1212@outlook.jp>
2021-10-31 00:24:02 +0000

    list upupdated packages

--- ec2/install_packages.sh
+++ ec2/install_packages.sh
@@ -6,9 +6,10 @@ packages="docker git make vim"
 #yum update -y
 
 for package in $packages; do
-	check=$(yum list installed | awk '{print $1}' | grep $package)
-	if [ ${#check} -eq 0 ]; then
-		echo $package " uninstalled!"
+	uninstalled=$(yum list installed | awk '{print $1}' | grep $package)
+	unupdated=$(yum list updates | awk '{print $1}' | grep $package)
+	if [ ${#uninstalled} -eq 0 ] || [ ${#unupdated} -gt 0 ]; then
+		echo $package " uninstalled or unupdated!"
 	fi
 done
 
